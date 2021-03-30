2b9d262c364cc476d592563d0c13afd3e5848a35
Владимир <nekro199107@gmail.com>
2021-03-30 22:49:31 +0300

    Настройка devServer

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.js',
-  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
   }
 };
