242ab98435221409a89c5abd34b65485c2efc01c
Владимир <nekro199107@gmail.com>
2021-05-04 13:35:04 +0300

    Дополнения к Smart-классу

--- src/view/smart.js
+++ src/view/smart.js
@@ -1,6 +1,11 @@
 import AbstractView from './abstract.js';
 
 export default class Smart extends AbstractView {
+  constructor() {
+    super();
+    this._state = {};
+  }
+
   restoreHandlers() {
     throw new Error('Abstract method not implemented: resetHandlers');
   }
@@ -8,25 +13,27 @@ export default class Smart extends AbstractView {
   updateElement() {
     const prevElement = this.getElement();
     const parent = prevElement.parentElement;
-    const prevElementScrollPosition = prevElement.scrollTop;
-    this.removeElement();
+    this.removeElement(); // Удаляет старый DOM
 
-    const newElement = this.getElement();
-    parent.replaceChild(newElement, prevElement);
-    newElement.scrollTop = prevElementScrollPosition;
-    this.restoreHandlers();
+    const newElement = this.getElement(); // Создает новый DOM
+    parent.replaceChild(newElement, prevElement); // Замена старого новым
+    this.restoreHandlers(); // Добавляет обработчики
   }
 
-  updateData(update) {
+  updateState(update, justStateUpdating) {
     if (!update) {
       return;
     }
 
-    this._data = Object.assign(
+    this._state = Object.assign(
       {},
       this._data,
       update,
     );
+
+    if (justStateUpdating) {
+      return;
+    }
 
     this.updateElement();
   }
